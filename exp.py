9c6a9f947395d6b1ebc8a851d794d20c7f905074
Ansh Arora <ansharora3839@gmail.com>
2023-08-05 12:41:41 +0530

    Created Registration Page of the website with validators

--- exp.py
+++ exp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import LoginForm, RegistrationForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '8562f33c5ce406479b245c3fec88f9fe'
 dummy_data = [
     {
         'author': 'Corey Schafer',
@@ -22,6 +22,22 @@ dummy_data = [
 @app.route("/home")
 def home_page():
     return render_template("home.html", posts=dummy_data)
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(
+            f'Successfully created an acccount for {form.username.data}!', "success")
+        return redirect(url_for('home_page'))
+    return render_template("register.html", form=form, title="Register")
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form, title="Login")
 
 
 if __name__ == "__main__":
