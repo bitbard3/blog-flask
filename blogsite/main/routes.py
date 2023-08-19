7d049b580f3901f9e4e1f9501788c1452ed023c8
bitbard3 <ansharora3839@gmail.com>
2023-08-19 15:35:22 +0530

    Add about page

--- blogsite/main/routes.py
+++ blogsite/main/routes.py
@@ -10,3 +10,8 @@ def home_page():
     posts = Post.query.order_by(
         Post.date_posted.desc()).paginate(page=page, per_page=6)
     return render_template("home.html", posts=posts)
+
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
