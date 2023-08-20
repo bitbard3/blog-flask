92686fbf559490616df3d1419f3dfc6668ced043
bitbard3 <ansharora3839@gmail.com>
2023-08-20 23:43:28 +0530

    Add environment variable

--- blogsite/config.py
+++ blogsite/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
