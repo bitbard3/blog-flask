461b665bfa7d11a84522131eebefb9cecf636c06
bitbard3 <ansharora3839@gmail.com>
2023-08-22 00:04:39 +0530

    Initialize the database for deployment

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from blogsite import create_app
+from blogsite import create_app, db
 app = create_app()
+with app.app_context():
+    db.create_all()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
